e8c26b1da1ccf8079bc68da3b11cdce5542cf076
jesusrube <jesusrube@example.com>
2025-08-29 01:29:57 +0200

    fix(ui): asegurar clic del botón triangular (z-index alto y pointer-events)

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
               <ExcelsiorHUD />
               <ExcelsiorHost />
             </div>
-            <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50">
+            <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-[9999] pointer-events-auto">
               <TriangleRealtimeButton />
             </div>
           </ExcelsiorProvider>
